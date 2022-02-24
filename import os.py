f48d2a46d45d98162aab461844c5d9c51ea13b08
André de Lemos <53500223+lemostrash@users.noreply.github.com>
2022-02-24 19:56:32 -0300

    Update import os.py

--- import os.py
+++ import os.py
@@ -2,7 +2,7 @@ import os
 
 print("=" * 60)
 
-ip_ou_host = input("Digite o Ip ou Host a ser verificado: ")
-print("-" * 60)
+ip_ou_host = input("Digita o Ip ou Host pra ser verificado: ")
+print("_" * 60)
 os.system('ping -n 6 {}'.format(ip_ou_host))
-print("-" * 60)
+print("_" * 60)
